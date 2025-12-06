ec3d4af677775312140c185c802c7dd5c520864f
uxu <uxuisusquared@proton.me>
2025-12-06 17:07:16 +0800

    add day02 part1&2

---
+++ 02/a2.py
@@ -0,0 +1,75 @@
+import sys; sys.dont_write_bytecode = True; from utils import *
+"""
+To do: ensure Code Runner works (in WSL), have preloaded the day and input in Chrome,
+saved input into the folder, have utils on the side, collapse regions
+Strings, lists, dicts:
+lmap, ints, positive_ints, floats, positive_floats, words, keyvalues
+
+Algorithms:
+bisect, binary_search, hamming_distance, edit_distance
+
+Data structures:
+Linked, UnionFind
+use deque for queue: q[0], q.append and q.popleft
+
+List/Vector operations:
+GRID_DELTA, OCT_DELTA
+lget, lset, fst, snd
+padd, pneg, psub, pmul, pdot, pdist1, pdist2sq, pdist2
+
+Matrices:
+matmat, matvec, matexp
+
+Previous problems:
+knot
+
+Dict things:
+dict.keys()
+dict.values()
+dict.items()
+"""
+
+def do_case(inp: str, sample=False):
+    # READ THE PROBLEM FROM TOP TO BOTTOM OK
+    def sprint(*a, **k): sample and print(*a, **k)
+
+    ranges = [s for s in inp.replace('\n', ',').split(',') if s]
+
+    ans = 0
+    for rg in ranges:
+        l, r = ints(rg)
+        for num in range(l, r+1):
+            s = str(num)
+            for period in range(len(s) // 2, 0, -1):
+                if len(s) % period != 0:
+                    continue
+                p = [s[i * period: (i+1) * period] for i in range(len(s) // period)]
+                if all([x == y for x, y in zip(p, p[1:])]):
+                    ans += num
+                    break
+
+    print(ans)
+    
+    return  # RETURNED VALUE DOESN'T DO ANYTHING, PRINT THINGS INSTEAD
+
+
+
+run_samples_and_actual([
+# Part 1
+r"""
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""], do_case)
