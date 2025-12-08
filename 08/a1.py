9251082bb4fbf53f909ee338fd588cbe3ae27bc4
uxu <uxuisusquared@proton.me>
2025-12-08 13:45:03 +0800

    add day08 part1

---
+++ 08/a1.py
@@ -0,0 +1,96 @@
+import sys; sys.dont_write_bytecode = True; from utils import *
+"""
+To do: ensure Code Runner works (in WSL), have preloaded the day and input in Chrome,
+saved input into the folder, have utils on the side, collapse regions
+Strings, lists, dicts:
+lmap, ints, positive_ints, floats, positive_floats, words, keyvalues
+
+Algorithms:
+bisect, binary_search, hamming_distance, edit_distance
+
+Data structures:
+Linked, UnionFind
+use deque for queue: q[0], q.append and q.popleft
+
+List/Vector operations:
+GRID_DELTA, OCT_DELTA
+lget, lset, fst, snd
+padd, pneg, psub, pmul, pdot, pdist1, pdist2sq, pdist2
+
+Matrices:
+matmat, matvec, matexp
+
+Previous problems:
+knot
+
+Dict things:
+dict.keys()
+dict.values()
+dict.items()
+"""
+
+def do_case(inp: str, sample=False):
+    # READ THE PROBLEM FROM TOP TO BOTTOM OK
+    def sprint(*a, **k): sample and print(*a, **k)
+
+    positions = [ints(line) for line in inp.splitlines()]
+    n = len(positions)
+    uf = UnionFind(n)
+
+    dists = []
+    for i in range(n):
+        for j in range(0, i):
+            dists.append((i, j, pdist2sq(positions[i], positions[j])))
+
+    for (i, j, _) in sorted(dists, key=lambda x: x[2])[:1000]:
+        uf.merge(i, j)
+
+    sizes = [0] * n
+    for i in range(n):
+        p = uf.find(i)
+        sizes[p] += 1
+    sizes_sorted = sorted(sizes, reverse=True)
+
+    ans = functools.reduce(lambda a, b: a * b, sizes_sorted[:3], 1)
+
+    print(ans)
+    return  # RETURNED VALUE DOESN'T DO ANYTHING, PRINT THINGS INSTEAD
+
+
+
+run_samples_and_actual([
+# Part 1
+r"""
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""], do_case)
